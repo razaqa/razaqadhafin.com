6c203082ab657e084e4271260cd95d55dabedfc8
Razaqa Dhafin <razaqa.haffiyan@tokopedia.com>
2023-04-03 13:29:34 +0700

    Add post data

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,95 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'is_headline',
+        'is_top_headline',
+        'body',
+        'user_id',
+        'category_id',
+        'is_published',
+        'pict',
+        'created_at'
+    ];
+
+    /**
+     * Bootstrap any application services.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            if(is_null($post->user_id)) {
+                $post->user_id = auth()->user()->id;
+            }
+        });
+
+        static::deleting(function ($post) {
+            $post->comments()->delete();
+            $post->tags()->detach();
+        });
+    }
+
+    /**
+     * Relation to Category model.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Relation to User model.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Relation to Tag model.
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    /**
+     * Relation to Comment model.
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Relation to Like model.
+     */
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function scopeDrafted($query)
+    {
+        return $query->where('is_published', false);
+    }
+
+    public function getPublishedAttribute()
+    {
+        return ($this->is_published) ? 'Yes' : 'No';
+    }
 }
