5bc63bf5cf26916e0559bf32fa8354500bd8669e
Razaqa Dhafin <razaqa.haffiyan@tokopedia.com>
2023-04-08 13:42:33 +0700

    Add login to google

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,14 @@ Route::get('/', [PageController::class, 'homepage'])->name('homepage');
 Route::post('/posts/{post}/comment', [PageController::class, 'comment'])->name('comment');
 Route::post('/posts/{post}/like', [PageController::class, 'like'])->name('like');
 Route::post('/message', [PageController::class, 'message'])->name('message');
+
+Route::get('redirect/{driver}', [LoginController::class, 'redirectToProvider'])
+    ->name('login.provider')
+    ->where('driver', implode('|', config('auth.socialite.drivers')));
+
+Route::get('{driver}/callback', [LoginController::class, 'handleProviderCallback'])
+    ->name('login.callback')
+    ->where('driver', implode('|', config('auth.socialite.drivers')));
 
 Route::get('/dashboard', function () {
     return view('dashboard');
