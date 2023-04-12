2b379f70e32bc998fb5c57b007a2c0c76bd7b9bc
Razaqa Dhafin <razaqa.haffiyan@tokopedia.com>
2023-04-12 10:04:56 +0700

    Add api routes for data

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/token', 'Api\AuthController@getAccessToken');
+Route::post('/auth/reset-password', 'Api\AuthController@passwordResetRequest');
+Route::post('/auth/change-password', 'Api\AuthController@changePassword');
+
+Route::group(['middleware' => 'auth:api', 'namespace' => 'Api'], function() {
+    Route::get('/tags', 'ListingController@tags');
+    Route::get('/categories', 'ListingController@categories');
+    Route::get('/users', 'ListingController@users')->middleware('admin');
+
+    Route::resource('/posts', 'PostController', ['only' => ['index', 'show']]);
+});
