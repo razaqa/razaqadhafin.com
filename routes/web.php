aad254bc8e9b0e89ac3851ab647e9da80be866df
Razaqa Dhafin <razaqa.haffiyan@tokopedia.com>
2023-04-12 10:04:42 +0700

    Add routes for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CKEditorController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CommentController;
+use App\Http\Controllers\Admin\MessageController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,14 +39,28 @@ Route::get('{driver}/callback', [LoginController::class, 'handleProviderCallback
     ->name('login.callback')
     ->where('driver', implode('|', config('auth.socialite.drivers')));
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::resource('/posts',  PostController::class);
+    Route::put('/posts/{post}/publish', [PostController::class, 'publish'])->middleware('admin');
+    Route::put('/posts/{post}/set-headline', [PostController::class, 'setHeadline'])->middleware('admin');
+    Route::put('/posts/{post}/set-homepage-headline', [PostController::class, 'setHomepageHeadline'])->middleware('admin');
+    Route::resource('/categories', CategoryController::class, ['except' => ['show']]);
+    Route::resource('/tags', TagController::class, ['except' => ['show']]);
+    Route::put('/tags/{tag}/set-for-work', [TagController::class, 'setForWork'])->middleware('admin');
+    Route::put('/tags/{tag}/unset-for-work', [TagController::class, 'unsetForWork'])->middleware('admin');
+    Route::resource('/comments', CommentController::class, ['only' => ['index', 'destroy']]);
+    Route::resource('/messages', MessageController::class, ['only' => ['index', 'destroy']]);
+    Route::resource('/users', UserController::class, ['middleware' => 'admin', 'only' => ['index', 'destroy']]);
+});
+
+Route::post('ckeditor/image_upload', [CKEditorController::class, 'upload'])->name('upload');
 
 require __DIR__.'/auth.php';
